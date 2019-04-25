87b629771358b1ca8a18975e2c41902dcf53f39e
JhoniFarias <34588750+JhoniFarias@users.noreply.github.com>
2019-04-24 23:47:08 -0300

    Update contador.js

--- project/js/contador.js
+++ project/js/contador.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
    	var cont = 0;
 	 $(setInterval(function(){
 	 	cont= cont+33;
-	    $('.numArbor').text(cont+"   Árvores desmatadas !");
+	    $('.numArbor').text(cont+"   Árvores derrubadas !");
 		 
 
 	  },1000));  
+
 });
    
