e4e7d1d81091bf42b22c24888d093d2ba2851a64
JhoniFarias <jhonifarias16@gmail.com>
2019-05-01 15:08:13 -0300

    updates

--- project/js/contador.js
+++ project/js/contador.js
@@ -1,11 +1,10 @@
-$(document).ready(function(){
+﻿$(document).ready(function(){
    	var cont = 0;
 	 $(setInterval(function(){
 	 	cont= cont+33;
-	    $('.numArbor').text(cont+"   Árvores derrubadas !");
+	    $('.numArbor').text(cont);
 		 
 
 	  },1000));  
-
 });
    
