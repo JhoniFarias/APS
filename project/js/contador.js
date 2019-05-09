d1340898440e6b3d4c2464569361e59b509663d9
JhoniFarias <jhonifarias16@gmail.com>
2019-05-09 18:25:45 -0300

    update of the week

--- project/js/contador.js
+++ project/js/contador.js
@@ -1,10 +1,12 @@
 ﻿$(document).ready(function(){
+
+	//Variavel de count
    	var cont = 0;
 	 $(setInterval(function(){
+	 	//Função para adicionar 33 em 33 no contador
 	 	cont= cont+33;
-	    $('.numArbor').text(cont);
-		 
-
+	    $('.numArbor').text(cont);		 
+//Numero maximo por segundo		 
 	  },1000));  
 });
    
